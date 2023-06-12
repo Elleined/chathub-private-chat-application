ab1ab9cdb18e5fc668ee47cfcf5336c661f6d79d
Elleined <demadegu@gmail.com>
2023-06-12 22:27:22 +0800

    Added userhandshakehandler

---
+++ src/main/java/com/privatechat/wsprivatechatapplication/UserHandshakeHandler.java
@@ -0,0 +1,22 @@
+package com.privatechat.wsprivatechatapplication;
+
+import com.sun.security.auth.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+public class UserHandshakeHandler extends DefaultHandshakeHandler {
+
+    @Override
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        String randId = UUID.randomUUID().toString();
+        log.debug("User with of id {} open the page", randId);
+        return new UserPrincipal(randId);
+    }
+}
--- src/main/java/com/privatechat/wsprivatechatapplication/config/WebSocketConfig.java
+++ src/main/java/com/privatechat/wsprivatechatapplication/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.privatechat.wsprivatechatapplication.config;
 
+import com.privatechat.wsprivatechatapplication.UserHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -12,14 +13,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket")
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("queue");
+        registry.enableSimpleBroker("/chat");
         registry.setApplicationDestinationPrefixes("/app");
     }
-
-
 }
