5a5f9b5a99762a17e7eb16f56b3cb541f11871c9
Elleined <demadegu@gmail.com>
2023-06-12 21:40:16 +0800

    Initial commit

---
+++ src/main/java/com/privatechat/wsprivatechatapplication/WsPrivateChatApplication.java
@@ -0,0 +1,13 @@
+package com.privatechat.wsprivatechatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPrivateChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPrivateChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privatechat/wsprivatechatapplication/WsPrivateChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privatechat.wsprivatechatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsPrivateChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
